dcb4d1136ae555a0c13a16d9fcba710eb7a702b9
Anlodar <35761452+Anlodar@users.noreply.github.com>
2018-12-12 09:55:12 +0100

    Add files via upload

--- Projet_Poo_2018_2019/src/tests/TestHCard.java
+++ Projet_Poo_2018_2019/src/tests/TestHCard.java
@@ -3,6 +3,8 @@
  */
 package tests;
 
+import java.io.File;
+
 import files.HCard;
 
 /**
@@ -15,9 +17,21 @@ public class TestHCard {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HCard card = new HCard("votrecarte.vcf");
-
+		
+		File file = new File("././votrecarte.vcf");
+		HCard card = new HCard(file);
+		
+		System.out.println(card.toString());
+		
+		System.out.println(card.getName());
+		System.out.println(card.getNumberHome());
+		System.out.println(card.getNumberWork());
+		System.out.println(card.getMail());
+		System.out.println(card.getAdressHome());
+		System.out.println(card.getAdressWork());
+		
+		card.toHtml();
+		System.out.println("HTML done");
 	}
 
 }
