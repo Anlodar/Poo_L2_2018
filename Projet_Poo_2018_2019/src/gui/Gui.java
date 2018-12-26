844cf278ec4cd9a11b2ce1f8055dca246bba9a77
QuentinLiguori <38524351+QuentinLiguori@users.noreply.github.com>
2018-12-26 19:09:45 +0100

    Add files via upload

--- Projet_Poo_2018_2019/src/gui/Gui.java
+++ Projet_Poo_2018_2019/src/gui/Gui.java
@@ -1,8 +1,6 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -21,68 +19,82 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import files.HCalendar;
-import files.HCalendarDictionnary;
 import files.HCard;
+import files.HEventTry;
 
+/**
+ * 
+ * @author Quentin
+ *
+ */
 public class Gui implements ActionListener{
-
-	File file;
-	static JFrame window;
-	JButton openbutton;
-	JTextArea textarea;
-	JFileChooser fc;
-	JScrollPane scroll;
-	static JSplitPane splitgauche;
-	FileFilter filterVCF;
-	FileFilter filterICS;
-	JPanel buttonpane;
-	HCard card;
-	HCalendar calendar;
+	
+	// Name All Variable used in Gui //
+	private File file;
+	private static JFrame window;
+	private JButton openbutton;
+	private JTextArea textarea;
+	private JFileChooser fc;
+	private JScrollPane scroll;
+	private static JSplitPane splitgauche;
+	private FileFilter filterVCF;
+	private FileFilter filterICS;
+	private JPanel buttonpane;
+	private HCard card;
+	private HCalendar calendar;
 	static JSplitPane splitPane;
-	JPanel Pane;
-	String type;
+	private String type;
 	
-	JPanel globalpane;
-	JPanel JTextpane;
-	JButton buttonmod;
-	JButton buttonser;		
-	JButton buttonhtml;
+	private JPanel globalpane;
+	private JPanel JTextpane;
+	private JButton buttonmod;
+	private JButton buttonser;		
+	private JButton buttonhtml;
 			
-	JComboBox<String> box;
-	String[] event;
-	String oldname;
+	private JComboBox<String> box;
+	private String[] event;
+	private String oldname;
 	
-	JTextField name;
-	JTextField number;
-	JTextField mail;
-	JTextField adressWork;
-	JTextField numberWork;
-	JTextField adressHome;
+	private JTextField name;
+	private JTextField number;
+	private JTextField mail;
+	private JTextField adressWork;
+	private JTextField numberWork;
+	private JTextField adressHome;
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	/**
+	 * Constructor Gui
+	 * <p> When an object is builded, initialize parameter wait the action to be done
+	 * 
+	 * @see JTextField#setMaximumSize(Dimension)
+	 * @see JTextArea#setEditable(boolean)
+	 * @see JFileChooser#setFileSelectionMode(int)
+	 * @see JFileChooser#setFileFilter(FileFilter)
+	 * @see JButton#addActionListener(ActionListener)
+	 * @see JComboBox#addActionListener(ActionListener)
+	 * @see JPanel#add(java.awt.Component)
+	 * @see JSplitPane#setDividerSize(int)
+	 */
 	public Gui() {
 		
+		// Initialize Panel And TextArea //
 		globalpane = new JPanel();
 		JTextpane = new JPanel();
 		buttonpane = new JPanel();
-		
 		textarea = new JTextArea(20,70);
-		textarea.setEditable(false);
 		
+		// Initialize FileChooser and its Filter //
 		fc = new JFileChooser();
 		filterVCF = new FileNameExtensionFilter("VCF File", "vcf");
 		filterICS = new FileNameExtensionFilter("ICS File", "ics");
-		fc.setFileSelectionMode(0);
-		fc.setFileFilter(filterVCF);
-		fc.setFileFilter(filterICS);
 		
+		// Initialize Buttons //
 		buttonmod = new JButton("Modifier");
-		buttonmod.addActionListener(this);
 		buttonser = new JButton("Serialiser");		
 		buttonhtml = new JButton("HTML");
 		openbutton = new JButton("Open File ...");
-		openbutton.addActionListener(this);
 		
+		// Initialize JTextField //
 		name = new JTextField("Name");
 		number = new JTextField("Number Home");
 		mail = new JTextField("Mail");
@@ -90,6 +102,26 @@ public class Gui implements ActionListener{
 		numberWork = new JTextField("Number Work");
 		adressHome = new JTextField("Adress Home");
 		
+		// Initialize the ComboBox and the scrollPane who contains the textarea //
+		box = new JComboBox();
+		scroll = new JScrollPane(textarea);
+		
+		// Initialize Box layouts for JTextpane and globalpane //
+		BoxLayout Box1 = new BoxLayout(JTextpane, 1);
+		BoxLayout Box2 = new BoxLayout(globalpane,1);
+		
+		// Disable the Editable on textarea and set Filter on FileChooser //
+		textarea.setEditable(false);
+		fc.setFileSelectionMode(0);
+		fc.setFileFilter(filterVCF);
+		fc.setFileFilter(filterICS);
+		
+		// Add ActionListener on object //
+		openbutton.addActionListener(this);
+		buttonmod.addActionListener(this);
+		box.addActionListener(this);
+		
+		// Set the Size of JTextFields //
 		name.setMaximumSize( new Dimension(250,20));
 		number.setMaximumSize( new Dimension(250,20));
 		numberWork.setMaximumSize( new Dimension(250,20));
@@ -97,10 +129,7 @@ public class Gui implements ActionListener{
 		adressHome.setMaximumSize( new Dimension(250,20));
 		adressWork.setMaximumSize( new Dimension(250,20));
 		
-		
-		box = new JComboBox();
-		box.addActionListener(this);
-		
+		// Add elements to corresponding Panel //
 		JTextpane.add(box);
     	JTextpane.add(name);
 		JTextpane.add(number);
@@ -108,64 +137,88 @@ public class Gui implements ActionListener{
 		JTextpane.add(mail);
 		JTextpane.add(adressHome);
 		JTextpane.add(adressWork);
-		
-		
 		buttonpane.add(buttonmod);
 		buttonpane.add(buttonser);
 		buttonpane.add(buttonhtml);
 		
-		
-		
-		BoxLayout Box1 = new BoxLayout(JTextpane, 1);
-		
+		// Set Layout on these panel //
 		JTextpane.setLayout(Box1);
-		
-		
-		
-		
-		BoxLayout Box2 = new BoxLayout(globalpane,1);
-		
 		globalpane.setLayout(Box2);
+		
+		// Add the Two panel into a global one //
 		globalpane.add(JTextpane);
 		globalpane.add(buttonpane);
 		
-		
-		scroll = new JScrollPane(textarea);
+		// Initialize the splitPane in the left part, fill it with a button and a scrollPane and set the divider size to 0 to lock it //
 		splitgauche = new JSplitPane(JSplitPane.VERTICAL_SPLIT, openbutton, scroll);
 		splitgauche.setDividerSize(0);
-		
-		
+		//Initialize the global splitPane , fill it with the left splitPane and the global panel and set the divider size to 0 to lock it //
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,splitgauche, globalpane);
 		splitPane.setDividerSize(0);
-		}
-		
-	
+	}
+	/**
+	 * @param file
+	 * Take a file and return the extension 
+	 * @return type the extension of the file
+	 */
 	public String getType(File file) {
-		
+		// Take the name of the file in entry and return the extension corresponding //
 		String type = file.getName().substring(file.getName().lastIndexOf("."));
-		
 		return type;
 	}
-	
+	/**
+	 * Wait an action to performed an appropriate answer, verify the type of the choosed file and adapt the gui 
+	 * 
+	 * @see ActionEvent#getSource()
+	 * @see JFileChooser#showOpenDialog(java.awt.Component)
+	 * @see Gui#getType(File)
+	 * @see Gui#equals(Object)
+	 * @see JTextArea#append(String)
+	 * @see JTextArea#setText(String)
+	 * @see JComboBox#removeAllItems()
+	 * @see JTextField#setText(String)
+	 * @see HCard#toString()
+	 * @see HCard#getName()
+	 * @see HCard#getNumberHome()
+	 * @see HCard#getNumberWork()
+	 * @see HCard#getMail()
+	 * @see HCard#getAdressHome()
+	 * @see HCard#getAdressWork()
+	 * @see JTextField#setEditable(boolean)
+	 * @see HCalendar#toString()
+	 * @see HCalendar#getAllSummaries()
+	 * @see JComboBox#addItem(Object)
+	 * @see HCalendar#searchEvent(String)
+	 * @see JComboBox#getSelectedItem()
+	 * @see JComboBox#toString()
+	 * @see HEventTry#getSummary()
+	 * @see HEventTry#getDateStart()
+	 * @see HEventTry#getDateEnd()
+	 * @see HEventTry#getDescription()
+	 * @see HEventTry#getLocation()
+	 * @see JTextField#getText()
+	 * @see HCard#modify(String, String, String, String, String, String)
+	 * @see HCalendar#modify(String, String, String, String, String, String)
+	 * @see HCalendar#toHtmlCalendar()
+	 * @see HCard#toHtml() 
+	 */
 	public void actionPerformed(ActionEvent e) {
-
-        //Handle open button action.
+        //Handle openbutton action.
         if (e.getSource() == openbutton) {
             int returnVal = fc.showOpenDialog(splitgauche);
 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 file = fc.getSelectedFile();
                 type = getType(file);
-                
-                
+               // Case where the file choosed is a Vcard //
                if (type.equals(".vcf")) {
-                	System.out.println("e");
                 	card = new HCard(file);
                 	textarea.setText("");
                 	textarea.append(card.toString());
                 	
                 	box.removeAllItems();
                 	
+                	// Set the current Variable in the JTextFields //
                 	name.setText(card.getName());
                 	number.setText(card.getNumberHome());
                 	numberWork.setText(card.getNumberWork());
@@ -173,9 +226,8 @@ public class Gui implements ActionListener{
                 	adressHome.setText(card.getMail());
             		adressWork.setText(card.getAdressWork());
             		adressWork.setEditable(true);
-            		
-            		
                }
+               //Handle the case where the file choosed is a VCalendar //
                else if(type.equals(".ics")) {
             	   
 	               	calendar = new HCalendar(file);
@@ -185,8 +237,12 @@ public class Gui implements ActionListener{
 	               	event = calendar.getAllSummaries();
 	               	
 	               	for(int i=0; i < event.length; i++) {
+	               		if(event[i] != null) {
 	               		box.addItem(event[i]);
+	               		}
 	               	}
+	               	
+	               	// Set the current variable in JTextFields
 	               	oldname = calendar.searchEvent(box.getSelectedItem().toString()).getSummary();
 	               	name.setText(oldname);
 		           	number.setText(calendar.searchEvent(box.getSelectedItem().toString()).getDescription());
@@ -198,36 +254,44 @@ public class Gui implements ActionListener{
               }
            }
        }
+        // Handle buttonmod action //
         else if(e.getSource() == buttonmod) {
+        	// Case where the file choosed is a VCard //
         	if(type.equals(".vcf")) {
-       				System.out.println("z");
        				card.modify(name.getText(), adressHome.getText(), adressWork.getText(), number.getText(), numberWork.getText(), mail.getText());
-       				System.out.println(card.getName());
        				textarea.setText("");
        				textarea.append(card.toString());
         	}
+        	// Case where the file choosed is a VCalendar //
         	else if(type.equals(".ics")) {
         		calendar.modify(oldname, name.getText(), numberWork.getText(), mail.getText(), adressHome.getText(), number.getText());
         		textarea.setText("");
         		textarea.append(calendar.toString());
         	}
         }
+        // Handle buttonser action //
        	else if(e.getSource() == buttonser) {
+       			// Case where the file choosed is a VCard //
        			if(type.equals(".vcf")) {
        				
        			}
+       			// Case where the file choosed is a VCalendar //
        			else if(type.equals(".ics")) {
        				
        			}
        	}
+        // Handle buttonhtml action //
        	else if(e.getSource() == buttonhtml) {
+        	// Case where the file choosed is a VCard //
        		if(type.equals(".vcf")) {
        			card.toHtml();
        		}
+        	// Case where the file choosed is a VCalendar //
        		else if(type.equals(".ics")) {
        			calendar.toHtmlCalendar();
        		}
        	}
+        // Handle the JComboBox choice of Event //
        	else if (e.getSource() == box) {
        		if(type.equals(".ics")) {
        			
@@ -243,19 +307,19 @@ public class Gui implements ActionListener{
        	}
               
 	}
+	/** 
+	 * @return splitgauche the left part of the GUI
+	 */
 	public JSplitPane getSplitGauche() {
 		
 		return splitgauche;
 	}
+	/**
+	 * @return splitPane the entire GUI
+	 */
 	public static JSplitPane getSplitPane() {
 		return splitPane;
 	}
-
-
-	public static void setSplitPane(JSplitPane splitPane) {
-		Gui.splitPane = splitPane;
-	}
-
 
 	public static void main(String[] args) {
 			
